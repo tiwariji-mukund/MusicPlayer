e3f51befdbbc7d0f8996c722d2beb217b925e8d8
Mukund Jee Tiwari <mukundjitiwari@outlook.com>
2023-11-24 22:48:10 +0530

    Songs list added

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+// songs
+
+const aloneSong = new Audio("./music/Alone.mp3");
+const fadedSong = new Audio("./music/Faded.mp3");
+const sandstormSong = new Audio("./music/First Fighting a Sandstorm.mp3");
+const loveSong = new Audio("./music/Love You Like A Love Song.mp3");
+const prettyGirlSong = new Audio("./music/Pretty Girl.mp3");
+
+const songs = [
+  {
+    name: "Faded",
+    artist: "Alan Walker",
+    music_path: fadedSong,
+    image: "./img.png",
+  },
+  {
+    name: "Alone",
+    artist: "Alan Walker",
+    music_path: aloneSong,
+    image: "./img.png",
+  },
+  {
+    name: "First Fighting a Sandstorm",
+    artist: "Sia",
+    music_path: sandstormSong,
+    image: "./img.png",
+  },
+  {
+    name: "Love You Like A Love Song",
+    artist: "Selena Gomez",
+    music_path: loveSong,
+    image: "./img.png",
+  },
+  {
+    name: "Pretty Girl",
+    artist: "Maggie Lindemann",
+    music_path: prettyGirlSong,
+    image: "./img.png",
+  },
+];
+
+function MusicPlayer() {
+  const artistName = document.getElementById("artist-name");
+  const songName = document.getElementById("song-name");
+  const image = document.getElementById("image");
+  const allSongs = document.getElementById("all-songs");
+  let current = 0 % songs.length;
+  let currentSong = songs[current].music_path;
+  songName.textContent = `${songs[current].name} - ${songs[current].artist}`;
+
+  // display all songs
+  songs.forEach((song) => {
+    const newSong = document.createElement("li");
+    newSong.classList.add("all-songs");
+    newSong.textContent = `${song.name} - ${song.artist}`;
+    allSongs.appendChild(newSong);
+  });
+
+  // music player
+  
+}
+
 // script for create playlist
 
 function createPlaylist(e) {
@@ -15,3 +77,14 @@ function createPlaylist(e) {
 document
   .getElementById("createPlaylistForm")
   .addEventListener("submit", createPlaylist);
+
+const addPlaylist = document.getElementById("add-btn");
+const currPlayList = document.querySelector(".currPlayList");
+addPlaylist.addEventListener("click", () => {
+  const song = document.createElement("li");
+  song.classList.add("name");
+  song.textContent = "Faded";
+  currPlayList.appendChild(song);
+});
+
+MusicPlayer();
